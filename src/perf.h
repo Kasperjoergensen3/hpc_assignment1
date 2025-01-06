f2017c0e3791175b677310cad63fbc4add54f0b8
Kasperjoergensen3 <Kasperjoergensen3@gmail.com>
2025-01-06 23:57:11 +0100

    commit

--- src/perf.h
+++ src/perf.h
@@ -1,2 +1,3 @@
 // function timer
 double timer(void (*func)(int, int, int, double **, double **, double **), int m, int n, int k, double **A, double **B, double **C);
+int check_matrix(int m, int n, double **A, double **B);
