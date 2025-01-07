08bbc7a0746c3d3e8f7747389ee9505a6d6f3705
Kasperjoergensen3 <kasperjoergensen3@gmail.com>
2025-01-07 22:53:09 +0100

    commit

---
+++ assignment1/code/jobs/compile_all.sh
@@ -0,0 +1,12 @@
+#remove all the executables and results
+make realclean
+
+#different optimization levels
+make TAG=_default              #No optimization
+make TAG=_O OPT="-g -O1"        #Optimization level 2
+make TAG=_O2 OPT="-g -O2"       #Optimization level 2
+make TAG=_O3 OPT="-g -O3"       #Optimization level 3
+make TAG=_Ofast OPT="-g -Ofast" #Optimization level fast
+
+#remove all the object files
+make clean
