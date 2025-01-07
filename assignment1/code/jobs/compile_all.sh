da2675f47e90ff6080086d778aba97a0efc1d3a4
Kasperjoergensen3 <Kasperjoergensen3@gmail.com>
2025-01-07 22:57:07 +0100

    commit

--- assignment1/code/jobs/compile_all.sh
+++ assignment1/code/jobs/compile_all.sh
@@ -2,8 +2,8 @@
 make realclean
 
 #different optimization levels
-make TAG=_default              #No optimization
-make TAG=_O OPT="-g -O1"        #Optimization level 2
+make TAG=_default               #No optimization
+make TAG=_O OPT="-g -O"         #Optimization level 1
 make TAG=_O2 OPT="-g -O2"       #Optimization level 2
 make TAG=_O3 OPT="-g -O3"       #Optimization level 3
 make TAG=_Ofast OPT="-g -Ofast" #Optimization level fast
