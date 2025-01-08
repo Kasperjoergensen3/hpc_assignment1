8b285140fc1ef48edb5fc5e61d5483131bea7238
Kasperjoergensen3 <kasperjoergensen3@gmail.com>
2025-01-08 12:51:27 +0100

    commit

--- assignment1/code/jobs/part2_1.sh
+++ assignment1/code/jobs/part2_1.sh
@@ -19,14 +19,14 @@ make clean
 
 mkdir -p outputs data/part2 # ensure that the directories exist
 
-DIMS="16 32 64 128 256 512 1024 2048 4096 8192"
+DIMS="16 32 64 128 256 512 1024 2048 4096 8192 16384 32768"
 ORDERS="lib mnk mkn nkm nmk kmn knm"
 
 #make double for loop to run the executables
 for dim in $DIMS; do
     for order in $ORDERS; do
         # Run the make command
-        make run_driver DIMS="$64 $64 $dim" ORDER=$order V=0 TAG=$TAG >>$DATAFILENAME
+        make run_driver DIMS="$128 $128 $dim" ORDER=$order V=0 TAG=$TAG >>$DATAFILENAME
     done
 done
 
