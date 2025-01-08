af786f3cfcb65091550d0e8f7d64f7b348f6cb34
Kasperjoergensen3 <Kasperjoergensen3@gmail.com>
2025-01-08 12:17:38 +0100

    commit

--- assignment1/code/jobs/create_plots.sh
+++ assignment1/code/jobs/create_plots.sh
@@ -1,2 +1,2 @@
-python scripts/visualize.py data/part1/lib_nat_compare.dat figures/part1.png  
-python scripts/visualize.py data/part2/ermuatition_compare.dat figures/part2.png  
+python scripts/visualize.py data/part1/lib_nat_compare.dat figures/part1.png
+python scripts/visualize.py data/part2/permuatition_compare.dat figures/part2.png
