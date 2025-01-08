723bd6789b87b4067bd9372e2f9ddcc22fba142f
Kasperjoergensen3 <kasperjoergensen3@gmail.com>
2025-01-08 09:41:49 +0100

    commit

--- assignment1/code/jobs/compile_all.sh
+++ assignment1/code/jobs/compile_all.sh
@@ -3,11 +3,11 @@
 make realclean
 
 #different optimization levels
-make TAG=_default               #No optimization
-make TAG=_O OPT="-g -O"         #Optimization level 1
-make TAG=_O2 OPT="-g -O2"       #Optimization level 2
-make TAG=_O3 OPT="-g -O3"       #Optimization level 3
-make TAG=_Ofast OPT="-g -Ofast" #Optimization level fast
+make TAG=default               #No optimization
+make TAG=O OPT="-g -O"         #Optimization level 1
+make TAG=O2 OPT="-g -O2"       #Optimization level 2
+make TAG=O3 OPT="-g -O3"       #Optimization level 3
+make TAG=Ofast OPT="-g -Ofast" #Optimization level fast
 
 #remove all the object files
 make clean
